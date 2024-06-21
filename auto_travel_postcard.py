e628bcc5087b3f51c969111a8ee6cb528bda88af
Li Yan <ylili@google.com>
2024-06-21 13:39:07 -0700

    update

--- auto_travel_postcard.py
+++ auto_travel_postcard.py
@@ -1,3 +1,15 @@
+def get_rgb_from_hexadecimal(color_rgb):
+	if color_rgb[0] == '#':
+		r = int(color_rgb[1:3], 16)
+		g = int(color_rgb[3:5], 16)
+		b = int(color_rgb[5:7], 16)
+		return r, g, b
+	else:
+		r = int(color_rgb[0:2], 16)
+		g = int(color_rgb[2:4], 16)
+		b = int(color_rgb[4:6], 16)
+		return r, g, b
+
 ### Show a dialog for inputing image
 from tkinter.filedialog import askopenfilename
 
@@ -12,30 +24,24 @@ import os
 genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+# identify city from image
 image_to_text_prompt = "Name of the city in the picture in one or two words"
 image1 = {
     'mime_type': 'image/jpeg',
     'data': pathlib.Path(input_image_path).read_bytes()
 }
-
 city_name_response = model.generate_content([image_to_text_prompt, image1])
 city_name = city_name_response.text
 print("City name is: ", city_name)
 
+# get color of the country the city belongs to
 city_color_prompt = f"What is the triditional color of country of the city {city_name} in one word"
 city_color_response = model.generate_content(city_color_prompt)
 city_color = city_color_response.text
 city_color_rgb_prompt = f"What is the rgb of color {city_color} into hexadecimal format, pick any one and return just one result"
 city_color_rgb_response = model.generate_content(city_color_rgb_prompt)
 city_color_rgb = city_color_rgb_response.text
-if city_color_rgb[0] == '#':
-	r = int(city_color_rgb[1:3], 16)
-	g = int(city_color_rgb[3:5], 16)
-	b = int(city_color_rgb[5:7], 16)
-else:
-	r = int(city_color_rgb[0:2], 16)
-	g = int(city_color_rgb[2:4], 16)
-	b = int(city_color_rgb[4:6], 16)
+r, g, b = get_rgb_from_hexadecimal(city_color_rgb)
 print(f"City color is: {city_color} {city_color_rgb} ({r},{g},{b})")
 
 ### Generate image
@@ -70,4 +76,3 @@ image_draw.text(
 	)
 
 image.show()
-
