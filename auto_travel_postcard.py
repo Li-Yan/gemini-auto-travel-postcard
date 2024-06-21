d07eef0cfccff15fde407e90f43cbb766d18068f
Li Yan <ylili@google.com>
2024-06-21 12:34:01 -0700

    update call chain

--- auto_travel_postcard.py
+++ auto_travel_postcard.py
@@ -1,31 +1,42 @@
 ### Show a dialog for inputing image
 from tkinter.filedialog import askopenfilename
 
-filename = askopenfilename()
-print("Input image file: ", filename)
+input_image_path = askopenfilename()
+print("Input image file: ", input_image_path)
 
 ### Sumary of input
 import google.generativeai as genai
+import pathlib
 import os
 
 genai.configure(api_key=os.environ["API_KEY"])
-
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-response = model.generate_content("What is name of the city in the picture")
+image_to_text_prompt = "What is name of the city in the picture"
+image1 = {
+    'mime_type': 'image/jpeg',
+    'data': pathlib.Path(input_image_path).read_bytes()
+}
 
-place_name = "Paris"
+response = model.generate_content([image_to_text_prompt, image1])
+city_name = response.text
 
-print(response.text)
+print("City name is: ", city_name)
 
 ### Generate image
 from vertexai.preview.vision_models import ImageGenerationModel
 
-image_prompt = f"please show a Generate a visually captivating and refreshing image of {place_name}"
-# image_prompt = "Generate a visually captivating and refreshing image of a glass of freshly squeezed lemonade against a vibrant background. The lemonade should be served in a tall, clear glass, with condensation dripping down the sides. Add a wedge of lemon and a sprig of mint for added freshness and visual appeal. Set the glass of lemonade against a vibrant gradient backdrop, creating a sense of depth and dimension. Ensure the lighting enhances the translucency of the lemonade and the texture of the lemon wedge. Experiment with vibrant color tones and a photorealistic art style to make the image pop."
+image_prompt = f"please show a oil paint image of {city_name}"
 print("Image prompt: ", image_prompt)
 
 model = ImageGenerationModel.from_pretrained("imagegeneration@005")
 images = model.generate_images(prompt=image_prompt)
 
 images[0].save(location="./gen-img.png", include_generation_parameters=True)
+
+### Display Image
+from PIL import Image
+
+image = Image.open('./gen-img.png')
+image.show()
+
